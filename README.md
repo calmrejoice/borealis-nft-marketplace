03774608d73aaf8541393059dd8ad7e9650bd0d4
Sim <simtj1990@gmail.com>
2022-02-19 23:57:41 +0800

    update readme

--- README.md
+++ README.md
@@ -11,8 +11,6 @@ Borealis is a NFT marketplace on Aurora chain. It facilitates the creating, buyi
 
 - CollectionFactory - 0xb3507D0FeF19a317d5ab10550Df66BD307C1b3A8
 - NFT Market - 0x5f6b7109C6132023F05a8a60824b4541a532BDD1
-- CollectionFactory OLD - 0x64964989E33617cBD2a2E9Db48fB5049b76820Af
-- NFT Market OLD - 0x5f0480244EdfEf51A9c8aDb4da80bE9cBD960f7b
 
 ## Features
 
--- config/contractAddresses.js
+++ config/contractAddresses.js
@@ -1,7 +1,6 @@
-// export const collectionFactoryAddress =
-//   '0x22AD2D56eBdC239e44867f037BaAc3f8fadfD633';
-// export const nftMarketAddress = '0x56EE7A05d53970c3c0321B432C17FE2c4033B7f8';
-
 export const collectionFactoryAddress =
   '0xb3507D0FeF19a317d5ab10550Df66BD307C1b3A8';
 export const nftMarketAddress = '0x5f6b7109C6132023F05a8a60824b4541a532BDD1';
+
+// - CollectionFactory OLD - 0x64964989E33617cBD2a2E9Db48fB5049b76820Af
+// - NFT Market OLD - 0x5f0480244EdfEf51A9c8aDb4da80bE9cBD960f7b
